f8a9303e64769a255d493363cf75a20eb70acf07
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-23 12:08:20 +0100

    Robin : CRUD prop AJS

--- Project_Swagg_Immo/src/main/webapp/scripts/proprietaire/providerProp.js
+++ Project_Swagg_Immo/src/main/webapp/scripts/proprietaire/providerProp.js
@@ -1,20 +1,21 @@
 /**
-* Provider AJS des services CRUD du propriétaire
-*/
+ * Provider AJS des services CRUD du propriétaire
+ */
 app.factory("propProvider", function($http) {
-	
+
 	/**
 	 * URL globale pour l'appel des services propriétaires
 	 */
 	var urlglobal = ""; /* METTRE L'URL QUAND SERVICE OKI */
-	
+
 	/**
-	 * ------------------------------------------------------------ Fonction pour ajouter un propriétaire
+	 * ------------------------------------------------------------ Fonction
+	 * pour ajouter un propriétaire
 	 */
 	function addProp(propForm, callback) {
 		$http({
 			method : 'POST',
-			url : urlglobal + '/ajouter',
+			url : urlglobal + '/ajouter', /* VERIFIER L'URL AVEC REST */
 			data : angular.toJson(propForm),
 			headers : {
 				'Content-Type' : 'application/json'
@@ -31,24 +32,87 @@ app.factory("propProvider", function($http) {
 
 	}
 	/**
-	 * -----------------------------------------------------------Fonction pour chercher un propriétaire
+	 * -----------------------------------------------------------Fonction pour
+	 * chercher un propriétaire
 	 */
-	function getProp(idProp, callback){
-		$http.get(urlglobal + '/proprietaire/' + idProp)
+	function getProp(idProp, callback) {
+		$http.get(urlglobal + '/proprietaire/' + idProp) /*
+															 * VERIFIER L'URL
+															 * AVEC REST
+															 */
 		.success(function(response) {
 			console.log(response);
 			callback(response);
-		})
-		.error(function(response) {
+		}).error(function(response) {
 			console.log(response.statusText);
 		})
 	}
+	/**
+	 * -----------------------------------------------------------Fonction pour
+	 * afficher tous les propriétaires
+	 */
+	function findAllProp(callback) {
+		$http.get(urlglobal + '/proprietaires/') /* VERIFIER L'URL AVEC REST */
+		.then(function successCallback(response) {
+			console.log(response.data);
+			callback(response);
+		}, function errorCallback(response) {
+			console.log("erreur : " + response.statusText);
+		});
+	}
+	/**
+	 * -----------------------------------------------------------Fonction pour
+	 * supprimer un propriétaires
+	 */
+	function deleteProp(id, callback){
+		$http({
+			method : 'DELETE',
+			url : urlglobal + '/delprop/'+id, /* VERIFIER L'URL AVEC REST */
+			})
+			.then(function successCallback(response) {
+				console.log(response.data);
+				console.log("successDelete");
+				callback(response);
+			}, function errorCallback(response) {
+				console.log("erreur : " + response.statusText);
+			});
+		
+	}
+	/**
+	 * -----------------------------------------------------------Fonction pour
+	 * modifier un propriétaire
+	 */
+	function updateProp(propUpdateForm, callback){
+		$http({
+			method : 'PUT',
+			url : urlglobal + '/proprietaire', /* VERIFIER L'URL AVEC REST */
+			data : angular.toJson(paysUpdate),
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+
+		})
+		.success(function(response) {
+			console.log(response);
+			callback(response);
+
+		}).error(function(response) {
+			console.log('Erreur : ' + response.statusText);
+
+		})
+
+	}
 	
-	
-	
-/** -------------------------------------------------------------Retour des fonctions du provider*/
+	/**
+	 * -------------------------------------------------------------Retour des
+	 * fonctions du provider
+	 */
 	return {
 		addProp : addProp,
+		getProp : getProp,
+		findAllProp : findAllProp,
+		deleteProp : deleteProp,
+		updateProp : updateProp
 	}
 
 })
