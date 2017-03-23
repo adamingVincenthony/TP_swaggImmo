c19d781d564ccb729792e88c6d0866b024183fcf
inti0277 <inti0277@DESKTOP-QVPF7VB.home>
2017-03-23 12:30:20 +0100

    javadoc ProprietaireRest

--- Project_Swagg_Immo/src/main/java/fr/adaming/rest/ProprietaireRest.java
+++ Project_Swagg_Immo/src/main/java/fr/adaming/rest/ProprietaireRest.java
@@ -18,19 +18,34 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
-
+/**
+ * annotations path et component du proprietaireRest
+ * @author inti0277
+ *
+ */
 @Component
 @Path("/proprietaire")
 public class ProprietaireRest {
 	
+	/**
+	 * injection automatique du propriétaire service
+	 */
 	@Autowired
 	private IProprietaireService proprietaireService;
 
+	/**
+	 * set du proprietaireService
+	 * @param proprietaireService
+	 */
 	public void setProprietaireService(IProprietaireService proprietaireService) {
 		this.proprietaireService = proprietaireService;
 	}
 	
 	
+	/**
+	 * méthode d'affichage de la liste des propriétaires
+	 * @return
+	 */
 	@GET
 	@Path("/findAll")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -38,6 +53,11 @@ public class ProprietaireRest {
 		return proprietaireService.findAllProprietaire();
 	}
 	
+	/**
+	 * méthode de recherche par id d'un propriétaire 
+	 * @param id
+	 * @return
+	 */
 	@GET
 	@Path("/getById/{id_param}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -45,6 +65,10 @@ public class ProprietaireRest {
 		return proprietaireService.getByIdProprietaire(id);
 	}
 	
+	/**
+	 * méthode d'ajout d'un propriétaire 
+	 * @param proprietaire
+	 */
 	@POST
 	@Path("/add")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -53,6 +77,10 @@ public class ProprietaireRest {
 		proprietaireService.addProprietaire(proprietaire);
 	}
 	
+	/**
+	 * méthode de mise à jour des données d'un propriétaire
+	 * @param proprietaire
+	 */
 	@PUT
 	@Path("/update")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -61,6 +89,10 @@ public class ProprietaireRest {
 		proprietaireService.updateProprietaire(proprietaire);
 	}
 	
+	/**
+	 * suppression d'un propriétaire
+	 * @param id
+	 */
 	@DELETE
 	@Path("/delete/{id_param}")
 	@Produces(MediaType.APPLICATION_JSON)
