d8d1ae059d92e34ad94c6ca7de64a71912a1b659
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-24 14:57:21 +0100

    routage

--- Project_Swagg_Immo/src/main/webapp/scripts/proprietaire/rootingProp.js
+++ Project_Swagg_Immo/src/main/webapp/scripts/proprietaire/rootingProp.js
@@ -5,24 +5,24 @@ app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 //================ Propiétaires ============================================
 	.when("/prop", {
-		templateUrl : "templates/main-content/Proprietaire/listProp.html",
+		templateUrl : "Project_Swagg_Immo/templates/main-content/Proprietaire/listProp.html",
 		controller : "findAllProp"
 	})
 	.when("/ajouterProp", {
-		templateUrl : "templates/main-content/Proprietaire/ajoutProp.html",
+		templateUrl : "Project_Swagg_Immo/templates/main-content/Proprietaire/ajoutProp.html",
 		controller : "addProp"
 	})
 
 	.when("/modifierProp", {
-		templateUrl : "templates/main-content/Proprietaire/updateProp.html",
+		templateUrl : "Project_Swagg_Immo/templates/main-content/Proprietaire/updateProp.html",
 		controller : "updateProp"
 	})
 	.when("/chercherProp", {
-		templateUrl : "templates/main-content/Proprietaire/findProp.html",
+		templateUrl : "Project_Swagg_Immo/templates/main-content/Proprietaire/findProp.html",
 		controller : "getProp"
 	})
 	.when("/supprimerProp", {
-		templateUrl : "templates/main-content/Proprietaire/deleteProp.html",
+		templateUrl : "Project_Swagg_Immo/templates/main-content/Proprietaire/deleteProp.html",
 		controller : "deleteProp"
 	})
 	.otherwise({
